c3e6319a4985a6309a9d68281b11c49c3a89ed88
VMurtis <v.murtis@gmail.com>
2026-07-27 12:03:07 -0300

    adiciona documentacao nos endpoints da api

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ app = FastAPI()
 
 @app.get('/api/hello')
 def hello_world():
+    '''
+    Endpoint que exibe uma mensagem incrível do mundo da programação!
+    
+    '''
     return {'Hello':'World'}
 
 @app.get('/api/restaurantes/')
 def get_restaurantes(restaurante: str = Query(None)):
+    '''
+    Endpoint para ver os cardápios dos restaurantes  
+    
+    '''
     url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
     response = requests.get(url)
 
